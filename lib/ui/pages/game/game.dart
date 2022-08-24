a03963c2c4df88f6135e6fee08859190fc79336b
Julian German <julian.german05@gmail.com>
2022-08-24 17:03:04 +0200

    Add frame to game base

--- lib/ui/pages/game/game.dart
+++ lib/ui/pages/game/game.dart
@@ -4,6 +4,7 @@ import 'package:b_ball/ui/global_widgets/custom_scaffold_wrapper.dart';
 import 'package:b_ball/ui/pages/game/game_model/game_model_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class Game extends StatefulWidget {
   static const String routeName = '/gameModule';
@@ -23,10 +24,7 @@ class _GameState extends State<Game> {
           if (state.gameModelStatus == GameModelStatus.loading) {
             return _buildLoader();
           }
-          return TextButton(
-              child: const Text('Back'),
-              onPressed: () =>
-                  context.read<GameModelCubit>().stopGame(context));
+          return _buildContent();
         }),
       ),
     );
@@ -44,7 +42,7 @@ class _GameState extends State<Game> {
             fontSize: fontSize,
           ),
         ),
-       FadeAnimatedText(
+        FadeAnimatedText(
           '2',
           duration: fadeInDuration,
           textStyle: CustomTypography.textStyleH1.copyWith(
@@ -62,6 +60,32 @@ class _GameState extends State<Game> {
       onFinished: () => context.read<GameModelCubit>().startGame(),
       totalRepeatCount: 1,
       pause: const Duration(milliseconds: 100),
+    );
+  }
+
+  Widget _buildContent() {
+    return Container(
+      padding: EdgeInsets.all(8.h),
+      child: Column(
+        children: [
+          SizedBox(height: 10.h),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 2,
+                  color: CustomColors.bottomBackGroundColor,
+                ),
+                borderRadius: BorderRadius.circular(10.r),
+              ),
+            ),
+          ),
+          TextButton(
+            child: const Text('Back'),
+            onPressed: () => context.read<GameModelCubit>().stopGame(context),
+          ),
+        ],
+      ),
     );
   }
 }
