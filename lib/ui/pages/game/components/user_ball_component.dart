caf48e115f758baabd31a08443fd7c32f3b1588d
Julian German <julian.german05@gmail.com>
2022-08-25 18:11:36 +0200

    Add basic movement

--- lib/ui/pages/game/components/user_ball_component.dart
+++ lib/ui/pages/game/components/user_ball_component.dart
@@ -1,16 +1,59 @@
-import 'dart:developer';
-
 import 'package:b_ball/config/colors.dart';
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
-import 'package:flame/extensions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:sensors_plus/sensors_plus.dart';
 
 class UserBallComponent extends PositionComponent with CollisionCallbacks {
-  static final _paint = Paint()..color = CustomColors.buttonColor;
+  late ShapeHitbox hitbox;
+  UserBallComponent(Vector2 position)
+      : super(
+          position: position,
+          size: Vector2.all(40),
+          anchor: Anchor.center,
+        );
+  List<double> _userGyroscopeValues = [];
+
   @override
-  void render(Canvas canvas) {
-    super.render(canvas);
-    canvas.drawCircle(Offset.zero, 20, _paint);
+  Future<void>? onLoad() async {
+    gyroscopeEvents.listen((GyroscopeEvent event) {
+      _userGyroscopeValues = <double>[event.x, event.y, event.z];
+    });
+
+    final defaultPaint = Paint()..color = CustomColors.buttonColor;
+    hitbox = CircleHitbox()
+      ..paint = defaultPaint
+      ..renderShape = true;
+    add(hitbox);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    if (_userGyroscopeValues.isNotEmpty) {
+      //TODO: Add boundaries!
+      position.y += _userGyroscopeValues[0] * 10.h;
+      position.x += _userGyroscopeValues[1] * 5.w;
+    }
+  }
+
+  @override
+  void onCollisionStart(
+    Set<Vector2> intersectionPoints,
+    PositionComponent other,
+  ) {
+    super.onCollisionStart(intersectionPoints, other);
+
+    if (other is ScreenHitbox) {
+      hitbox.paint.color = Colors.red;
+    }
+  }
+
+  @override
+  void onCollisionEnd(PositionComponent other) {
+    super.onCollisionEnd(other);
+
+    hitbox.paint.color = CustomColors.buttonColor;
   }
 }
--- lib/ui/pages/game/widgets/game_base.dart
+++ lib/ui/pages/game/widgets/game_base.dart
@@ -1,22 +1,19 @@
 import 'dart:async';
-
 import 'package:b_ball/ui/pages/game/components/user_ball_component.dart';
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
-import 'package:flame/input.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:sensors_plus/sensors_plus.dart';
 
-class GameBase extends FlameGame with HasCollisionDetection, TapDetector {
-  List<double> _userGyroscopeValues = [];
-  UserBallComponent userBall = UserBallComponent();
-
+class GameBase extends FlameGame with HasCollisionDetection {
   @override
   Color backgroundColor() => Colors.transparent.withOpacity(0.2);
 
   @override
   Future<void>? onLoad() async {
+    // Add colision
+    add(ScreenHitbox());
+    UserBallComponent userBall = UserBallComponent(size / 2);
+
     await super.onLoad();
     // Inside the game methods:
     const pauseOverlayIdentifier = 'PauseMenu';
@@ -24,22 +21,7 @@ class GameBase extends FlameGame with HasCollisionDetection, TapDetector {
     // Marks 'PauseMenu' to be rendered.
     overlays.add(pauseOverlayIdentifier);
 
-    userBall = UserBallComponent()
-      ..position = size / 2
-      ..anchor = Anchor.center;
-    gyroscopeEvents.listen((GyroscopeEvent event) {
-      _userGyroscopeValues = <double>[event.x, event.y, event.z];
-    });
     // START GAME:
     add(userBall);
-  }
-
-  @override
-  void update(double dt) {
-    super.update(dt);
-    if (_userGyroscopeValues.isNotEmpty) {
-      userBall.y += _userGyroscopeValues[0] * 10.h;
-      userBall.x += _userGyroscopeValues[1] * 5.w;
-    }
   }
 }
